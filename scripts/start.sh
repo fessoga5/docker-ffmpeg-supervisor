e9ac6be80ce718c312da9f43f744367477917aae
Artem Karamyshev <fessoga5@gmail.com>
2016-08-04 10:26:23 +0800

    Update: Add timeout! Add retries! Add start cron and syslog! Add loging ffmpeg in syslog!

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,6 +8,12 @@ if [ ! -z "$PASSWORD" ] ; then
     echo "ubuntu:${PASSWD}" | /usr/sbin/chpasswd
 fi
 
+# Start syslog
+/usr/sbin/rsyslogd
+
+# Start cron
+/usr/sbin/cron
+
 #   1. transkoding mpeg2 to mpeg4 for translate on web site
 #   ffmpeg -threads 1 -loglevel warning -i udp://238.0.1.2:1234 -vcodec h264 -vsync passthrough -r 25 -vb 2000k -preset fast -acodec aac -strict experimental -threads 1 -f mpegts "udp://127.0.0.1:1051?pkt_size=1316&ttl=50"
 #   env: type=mp2tomp4
@@ -29,23 +35,23 @@ DESTURLE=${DESTURL:="udp://127.0.0.1:1234"}
 
 case ${TYPEE} in
 mp2tomp4)
-    TEMPLATE_LIST="ffmpeg -threads 1 -loglevel warning -i ${URLE} -vcodec h264 -vsync passthrough -r 25 -vb 2000k -preset fast -acodec aac -strict experimental -threads 1 -f mpegts ${DESTURLE}"
+    TEMPLATE_LIST="ffmpeg -threads 1 -loglevel warning -timeout 3 -i ${URLE} -vcodec h264 -vsync passthrough -r 25 -vb 2000k -preset fast -acodec aac -strict experimental -threads 1 -f mpegts ${DESTURLE}"
     ;;
 mp4tomp4)
-    TEMPLATE_LIST="ffmpeg -threads 1 -loglevel warning -i ${URLE} -vcodec copy -acodec aac -strict experimental -threads 1 -f mpegts ${DESTURLE}"
+    TEMPLATE_LIST="ffmpeg -threads 1 -loglevel warning -timeout 3 -i ${URLE} -vcodec copy -acodec aac -strict experimental -threads 1 -f mpegts ${DESTURLE}"
     ;;
 rtsptranslate)
-    TEMPLATE_LIST="ffmpeg -stimeout 5000000 -rtsp_transport tcp -i ${URLE} -vcodec copy - a codec aac -strict experimental -ab 32k -f mpegts ${DESTURLE}"
+    TEMPLATE_LIST="ffmpeg -threads 1 -loglevel warning -stimeout 5000000 -rtsp_transport tcp -i ${URLE} -vcodec copy - a codec aac -strict experimental -ab 32k -f mpegts ${DESTURLE}"
     ;;
 rtspsave)
-    TEMPLATE_LIST="ffmpeg -stimeout 5000000 -rtsp_transport tcp -i ${URLE} -vcodec copy - a codec aac -strict experimental -ab 32k -y -f segment -segment_time 300 -segment_atclocktime 1 -reset_timestamps 1 -strftime 1 /share/%y%m%d%h%m%s.mp4"
+    TEMPLATE_LIST="ffmpeg -threads 1 -loglevel warning -stimeout 5000000 -rtsp_transport tcp -i ${URLE} -vcodec copy - a codec aac -strict experimental -ab 32k -y -f segment -segment_time 300 -segment_atclocktime 1 -reset_timestamps 1 -strftime 1 /share/%y%m%d%h%m%s.mp4"
     ;;
 esac
 
-echo $TEMPLATE_LIST
+logger -t "docker-ffmpeg-supervisor" "Start command: $TEMPLATE_LIST"
 
-#sed -i.bak 's/template_ffmpeg/'"$TEMPLATE_LIST"'/g' 
-echo "command=$TEMPLATE_LIST" >> /etc/supervisor/supervisord.conf
+TEMPLATE_SUPERVISOR="[program:ffmpeg]\ncommand=$TEMPLATE_LIST\nautostart=true\nautorestart=true\nstartretries=100000000\nstdout_logfile=/dev/stdout\nstdout_logfile_maxbytes=0\nstderr_logfile=syslog\n"
+printf $TEMPLATE_SUPERVISOR > /etc/supervisor/conf.d/ffmpeg.conf
 
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
